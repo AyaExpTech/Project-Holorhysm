03b8d93c0822f58c7ddf56c2db9b0c42f25c2536
AyaExpTech <ayasakaexpresstech@gmail.com>
2023-03-03 00:35:17 +0900

    ver 0.1.0-beta.6 - 『AXT-func.js』と『conv_JSONC-to-JSON.js』を前提外部スクリプトとして追加読み込み     - これに伴いtitle.jsからconv_JSONC-to-JSON.min.jsの内容を削除 - 楽曲リスト画面でプレイ開始ボタンが動くかの動作確認を可能にするための仮ボタン追加     - ついでにボタン押したっぽいアニメーションをつけるCSSを追加 - プレイ開始ボタン押下時に画面が#playに遷移するように     - #playにはまだなにもない

--- script/list.js
+++ script/list.js
@@ -6,8 +6,8 @@
 const select = (musicID, tendency) => {
     document.querySelector("#fade").classList.add("fading");
     window.setTimeout(() => {
-        document.querySelector("#title").classList.remove("using");
-        document.querySelector("#list").classList.add("using");
+        document.querySelector("#list").classList.remove("using");
+        document.querySelector("#play").classList.add("using");
         document.querySelector("#fade").classList.remove("fading");
     }, 200);
     const playingMusic = musicList.all.addition[musicID];
@@ -15,4 +15,8 @@ const select = (musicID, tendency) => {
     const musicFile = musicList[playingMusic][playingScore].music;
     const scoreFile = musicList[playingMusic][playingScore].score;
     const scoreOffset = musicList[playingMusic][playingScore].offset;
+    console.log(musicFile, scoreFile, scoreOffset);
+    // TODO : 2ファイルをダウンロードしてきてObjectにしてplay.jsのほうに書くであろうプレイ(メイン)関数に投げる
+    // 綾坂こと「ファイル名は取得できてるやで」
+    // 綾坂こと「"Promiseなんもわからん"になってるので理解してから続きを書く所存」
 }
--- script/title.js
+++ script/title.js
@@ -1,9 +1,4 @@
 /**
- * JSONC→JSONパーサー
- */
-const JSONC_to_JSON = i => { let s = 0, o = ""; for (let j = 0; j < i.length; j++) { let q = i[j]; if (s == 1) { q == "\"" ? s = 0 : 0; o += q; continue } if (s == 2) { q == "\n" ? (s = 0, o += q) : 0; continue } if (s == 3) { q + i[j + 1] == "*/" ? (s = 0, j += 1) : 0; continue } if (q == "\\") { o += q, j += 1, o += i[j]; continue } if (q == "/") { i[j + 1] == "/" ? (s = 2, j += 1) : 0; i[j + 1] == "*" ? (s = 3, j += 1) : 0; continue } if (q == "\"") { (s = 1, o += q); continue } o += q; continue } return o }
-
-/**
  * ページ読み込み時の処理
  */
 window.addEventListener("load", function () {
@@ -33,9 +28,7 @@ xhr.open("GET", "score/list.jsonc");
 xhr.responseType = "text"; // text, arraybuffer, blob, document, jsonが使用可能。jsonのresponseは自動パースされる
 xhr.send();
 xhr.onload = () => {
-    if (xhr.status != 200) {
-        console.log("取得に失敗");
-    } else {
+    if (xhr.status == 200) {
         musicList = JSON.parse(JSONC_to_JSON(xhr.response));
     }
 };
